3d76b13ce5c2b7d9e1bf2cb516fc96f69b701c3b
Omer Sapir <omersapir15@gmail.com>
2024-05-15 10:44:51 +0300

    fixing

--- Day04/counter.py
+++ Day04/counter.py
@@ -1,7 +1,9 @@
+import sys
+
 def count(file_name):
     count_dict = {'characters': 0, 'words': 0, 'lines': 0} 
-    with open(file_name) as fn:
-        text = fn.read()
+    with open(file_name, 'r') as f:
+        text = f.read()
         for ch in text:
             count_dict['characters']+=1
             if ch=='\n':
@@ -10,4 +12,17 @@ def count(file_name):
             count_dict['lines']+=1 #last line without '\n'
         for w in text.split():
             count_dict['words']+=1
-    print(count_dict)
+    return count_dict
+
+
+
+def main():
+    if len (sys.argv)!=2:
+        exit(f"Usage: {sys.argv[0]} FileName")
+        
+    file_name = sys.argv[1]
+    result = count(file_name)
+    print(result)
+
+
+main()
